797a6838d2203a79030bea9694ee396508c62a29
Federico Andrucci <federico.andrucci@gmail.com>
2023-08-22 15:17:51 +0200

    missing ;

--- PathTracing/src/Input.h
+++ PathTracing/src/Input.h
@@ -36,7 +36,7 @@ public:
     static bool IsKeyDown(KeyCode keycode);
     static bool IsMouseButtonDown(MouseButton button);
     static glm::vec2 GetMousePosition();
-    static void SetCursorMode(CursorMode cursorMode)
+    static void SetCursorMode(CursorMode cursorMode);
 };
 
 
