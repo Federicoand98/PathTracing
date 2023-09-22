add299b02683ce9127ff33ec22eeeb050fcdf359
Federico Andrucci <federico.andrucci@gmail.com>
2023-09-22 17:59:34 +0200

    test

--- PathTracing/src/Renderer.cpp
+++ PathTracing/src/Renderer.cpp
@@ -74,6 +74,7 @@ void Renderer::DrawSceneQuad() {
              1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
              1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
         };
+        
         // setup plane VAO
         glGenVertexArrays(1, &m_QuadVAO);
         glGenBuffers(1, &m_QuadVBO);
