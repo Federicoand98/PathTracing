e4013245d48f418209568d39d7994436bddf9d2a
Federico Andrucci <federico.andrucci@gmail.com>
2024-02-06 16:27:27 +0100

    refactor

--- CNN-Denoiser/prediction.py
+++ CNN-Denoiser/prediction.py
@@ -10,7 +10,6 @@ def patches(img, patch_size):
     patches = patchify(img, (patch_size, patch_size, 3), step=patch_size)
     return patches
 
-#Custom function to get denoised image prediction for noisy images
 def prediction(img, model):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img,(1024,1024))
@@ -31,7 +30,6 @@ def prediction(img, model):
     return pred_img
 
 
-#Custom function to get denoised image prediction for noisy images on quantized models using tflite
 def prediction_tflite(img, model, input_details, output_details):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img,(1024,1024))
@@ -58,7 +56,6 @@ def prediction_tflite(img, model, input_details, output_details):
     return pred_img
 
 
-#Custom function that computes the psnr and ssim values for images
 def psnr_and_ssim(X_test,y_test,model,model_type='Normal'):
     psnr_nsy = 0.0
     psnr_de_nsy = 0.0
@@ -66,30 +63,24 @@ def psnr_and_ssim(X_test,y_test,model,model_type='Normal'):
     ssim_de_nsy = 0.0
 
     for i in range(len(X_test)):
-        #getting the noisy images
         path = X_test.iloc[i]
         nsy = cv2.imread(path)  
 
-        #getting the predicted images
         if model_type == 'Quantized': 
             pred = prediction_tflite(nsy,model)
         else:
             pred = prediction(nsy,model)
 
-        #getting the ground truth images
         path = y_test.iloc[i]
         gt = cv2.imread(path)         
         gt = cv2.cvtColor(gt, cv2.COLOR_BGR2RGB)
 
-        #Resizing the images
         gt = cv2.resize(gt,(1024,1024))
         nsy = cv2.resize(nsy,(1024,1024))
 
-        #Normalizing the images
         gt = gt.astype("float32") / 255.0
         nsy = nsy.astype("float32") / 255.0
 
-        #Computing psnr and ssim for test images
         psnr_nsy += psnr(gt,nsy)
         psnr_de_nsy += psnr(gt,pred)
         ssim_nsy += ssim(gt,nsy,multichannel=True,data_range=nsy.max() - nsy.min())
